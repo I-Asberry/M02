dc811850c604a56aaab6fb7d8a8f44be7f047d5c
Isaac A <iasberry@ivytech.edu>
2023-09-01 15:27:22 -0400

    Final version for submitting

--- M02_If-Else-While_Lab.py
+++ M02_If-Else-While_Lab.py
@@ -1,4 +1,21 @@
 #Isaac Asberry
-#GPA Calculator
+#Dean's List/Honor Roll Verifier
 #This program will accept user inputs for first and last name, along with their GPA, to output wether they made Dean's List or Honor Roll or none
 
+last_name = input("Last name: ")
+
+while last_name != "ZZZ" and last_name != "zzz":  
+    first_name = input("First name: ")
+    stu_gpa = float(input("GPA: ")) 
+
+    if stu_gpa >= 3.5:
+        print("""You have made the Dean's List! \nType "ZZZ" or "zzz" to quit.""")
+        last_name = input("Last name: ")
+
+    elif stu_gpa >= 3.25 and stu_gpa < 5:
+        print("""You have made the Honor Roll! \nType "ZZZ" or "zzz" to quit.""")
+        last_name = input("Last name: ")
+
+    else:
+        print("""You have not made the Dean's List or Honor Roll. \nType "ZZZ" or "zzz" to quit.""")
+        last_name = input("Last name: ")
